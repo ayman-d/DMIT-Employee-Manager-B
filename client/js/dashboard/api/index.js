d5cc6aa34a5b0539a6022cc9de43719a2e231393
ayman-d <aymand1982@gmail.com>
2021-03-12 00:04:11 -0700

    adjust api URL

--- client/js/dashboard/api/index.js
+++ client/js/dashboard/api/index.js
@@ -2,27 +2,16 @@
 // developmentUrl = 'http://localhost:5000/
 
 const api = {
-  getAllDepartments: "http://localhost:5000/api/departments/",
+  getAllDepartments:
+    "https://employee-manager-ayman.herokuapp.com/api/departments/",
   // Snippet #5
-  updateEmployee: `http://localhost:5000/api/departments/employee/update`,
+  updateEmployee: `https://employee-manager-ayman.herokuapp.com/api/departments/employee/update`,
   getDepartment: (name) => {
-    return `http://localhost:5000/api/departments/name/${name}`;
+    return `https://employee-manager-ayman.herokuapp.com/api/departments/name/${name}`;
   },
   deleteEmployee: (id) => {
-    return `http://localhost:5000/api/departments/employee/id/${id}`;
+    return `https://employee-manager-ayman.herokuapp.com/api/departments/employee/id/${id}`;
   },
 };
-
-// const api = {
-//     getAllDepartments: 'https://employee-managers.herokuapp.com/api/departments/',
-//     // Snippet #5
-//     updateEmployee: `https://employee-managers.herokuapp.com/api/departments/employee/update`,
-//     getDepartment: (name)=>{
-//         return `https://employee-managers.herokuapp.com/api/departments/name/${name}`
-//     },
-//     deleteEmployee: (id)=>{
-//         return `https://employee-managers.herokuapp.com/api/departments/employee/id/${id}`
-//     }
-// }
 
 export default api;
